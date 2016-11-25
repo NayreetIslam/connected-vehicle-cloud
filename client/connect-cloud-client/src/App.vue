f78dd8dabd9c55c9f883f44f51f23a8139abc8bc
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2016-11-25 15:25:50 -0500

    Add initial code base for the web app

---
+++ client/connect-cloud-client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <ServerLog v-bind:log="log"></ServerLog>
+  </div>
+</template>
+
+<script>
+import ServerLog from './components/ServerLog'
+
+export default {
+  name: 'app',
+  components: {
+    ServerLog
+  },
+
+  data () {
+    return {
+      log: ''
+    }
+  },
+
+  created () {
+    setInterval(this.ping, 1000)
+  },
+
+  methods: {
+    ping () {
+      this.log += 'pinging 192.168.1.1\n'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/connect-cloud-client/src/components/ServerLog.vue
@@ -0,0 +1,16 @@
+<template>
+  <textarea id="serverlog" cols="50" rows="5">{{ log }}</textarea>
+</template>
+
+<script>
+export default {
+  name: 'ServerLog',
+  props: ['log']
+}
+</script>
+
+<style>
+#serverlog {
+
+}
+</style>
