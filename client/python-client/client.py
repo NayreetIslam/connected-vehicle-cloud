df893635dd961d890382184f68df71f56b23bb84
Dylan Kauling <gunsmithy@gmail.com>
2016-11-27 10:11:56 -0500

    Heavily revised Docker implementation
    Added support for ARM devices with Docker. Split server into separate
    file. Two separate docker files now, one for each architecture. Updated
    the client code to support passing in a server address instead of being
    hardcoded. Updated client Dockerfile to support passing in a server IP.
    Updated the Readmes with corrected commands for running the various
    Docker containers.

--- client/python-client/client.py
+++ client/python-client/client.py
@@ -3,6 +3,8 @@
 import asyncio
 import websockets
 import json
+import sys
+import getopt
 
 # async def handler(websocket, path):
 #   while True:
@@ -11,36 +13,49 @@ import json
 #     await websocket.send(message)
 
 async def writeFile(filename, content):
-  return json.dumps({
-    'type': 'write',
-    'filename': filename,
-    'payload': content,
-  })
+    return json.dumps({
+        'type': 'write',
+        'filename': filename,
+        'payload': content,
+    })
 
 async def readFile(filename):
-  return json.dumps({
-    'type': 'read',
-    'payload': filename,
-  })
+    return json.dumps({
+        'type': 'read',
+        'payload': filename,
+    })
 
-async def client():
-  async with websockets.connect('ws://localhost:8765') as websocket:
+async def client(argv):
+    address = '127.0.0.1'
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["ip="])
+    except getopt.GetoptError:
+        print ('client.py -i <serverAddress>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ip"):
+            address = arg
+    print ('Server address is: ', address + '\n')
 
-    # Write a file
-    filename = input('Writing to a file...\nEnter a filename:')
-    content = input('Enter the content of the file:')
-    print('\nSending a request to save the data to a file...\n')
-    data = await writeFile(filename, content)
-    await websocket.send(data)
-    response = await websocket.recv()
-    print("Response:\n{}".format(response))
+    async with websockets.connect('ws://' + address + ':8765') as websocket:
+        # Write a file
+        filename = input('Writing to a file...\nEnter a filename:')
+        content = input('Enter the content of the file:')
+        print('\nSending a request to save the data to a file...\n')
+        data = await writeFile(filename, content)
+        await websocket.send(data)
+        response = await websocket.recv()
+        print("Response:\n{}".format(response))
 
-    # Read a file
-    print('Sending a request to read the file...')
-    data = await readFile(filename)
-    await websocket.send(data)
-    response = await websocket.recv()
-    print("Response:\n{}".format(response))
+        # Read a file
+        print('Sending a request to read the file...')
+        data = await readFile(filename)
+        await websocket.send(data)
+        response = await websocket.recv()
+        print("Response:\n{}".format(response))
 
-asyncio.get_event_loop().run_until_complete(client())
+asyncio.get_event_loop().run_until_complete(client(sys.argv[1:]))
 # asyncio.get_event_loop().run_forever()
