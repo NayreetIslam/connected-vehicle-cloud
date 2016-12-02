889bebad94ba072181106e5f4e2126e7a18bef2b
Dylan Kauling <gunsmithy@gmail.com>
2016-12-02 13:41:15 -0500

    File formatting for PEP 8 compliance
    I don't like the little yellow lines in PyCharm okay?
    Also added Pycharm project .idea folder to the gitignore.

--- client/python-client/client.py
+++ client/python-client/client.py
@@ -12,14 +12,14 @@ import getopt
 #     print('message:\n'.format(message))
 #     await websocket.send(message)
 
-async def writeFile(filename, content):
+async def write_file(filename, content):
     return json.dumps({
         'type': 'write',
         'filename': filename,
         'payload': content,
     })
 
-async def readFile(filename):
+async def read_file(filename):
     return json.dumps({
         'type': 'read',
         'payload': filename,
@@ -28,31 +28,31 @@ async def readFile(filename):
 async def client(argv):
     address = '127.0.0.1'
     try:
-        opts, args = getopt.getopt(argv,"hi:",["ip="])
+        opts, args = getopt.getopt(argv, "hi:", ["ip="])
     except getopt.GetoptError:
-        print ('client.py -i <serverAddress>')
+        print('client.py -i <serverAddress>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+            print('test.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ("-i", "--ip"):
             address = arg
-    print ('Server address is: ', address + '\n')
+    print('Server address is: ', address + '\n')
 
     async with websockets.connect('ws://' + address + ':8765') as websocket:
         # Write a file
         filename = input('Writing to a file...\nEnter a filename:')
         content = input('Enter the content of the file:')
         print('\nSending a request to save the data to a file...\n')
-        data = await writeFile(filename, content)
+        data = await write_file(filename, content)
         await websocket.send(data)
         response = await websocket.recv()
         print("Response:\n{}".format(response))
 
         # Read a file
         print('Sending a request to read the file...')
-        data = await readFile(filename)
+        data = await read_file(filename)
         await websocket.send(data)
         response = await websocket.recv()
         print("Response:\n{}".format(response))
--- server/server.py
+++ server/server.py
@@ -5,41 +5,47 @@ import websockets
 import json
 import aiofiles
 
-async def writeFile(command):
-  async with aiofiles.open(command['filename'], mode='w') as f:
-    await f.write(command['payload'])
 
-async def readFile(command):
-  async with aiofiles.open(command['payload'], mode='r') as f:
-    return await f.read()
+async def write_file(command):
+    async with aiofiles.open(command['filename'], mode='w') as f:
+        await f.write(command['payload'])
+
+
+async def read_file(command):
+    async with aiofiles.open(command['payload'], mode='r') as f:
+        return await f.read()
+
 
 async def ping(command):
     return 'pong'
 
+
 async def handler(websocket, path):
-  while True:
-    try:
-      message = await websocket.recv()
-      response = await processCommand(message)
-      await websocket.send(json.dumps({
-        'type': 'success',
-        'payload': response if response is not None else '',
-      }))
-    except websockets.exceptions.ConnectionClosed:
-      # print("Client disconnected")
-      pass
+    while True:
+        try:
+            message = await websocket.recv()
+            response = await process_command(message)
+            await websocket.send(json.dumps({
+                'type': 'success',
+                'payload': response if response is not None else '',
+            }))
+        except websockets.exceptions.ConnectionClosed:
+            # print("Client disconnected")
+            pass
 
-async def processCommand(command):
-  print("< {}".format(command))
-  commandReceived = json.loads(command)
 
-  options = {
-    'write': writeFile,
-    'read': readFile,
-    'ping': ping,
-  }
+async def process_command(command):
+    print("< {}".format(command))
+    command_received = json.loads(command)
 
-  return await options[commandReceived['type']](commandReceived)
+    options = {
+        'write': write_file,
+        'read': read_file,
+        'ping': ping,
+    }
+
+    return await options[command_received['type']](command_received)
+
 
 start_server = websockets.serve(handler, '0.0.0.0', 8765)
 print("Server listening on port 8765")
