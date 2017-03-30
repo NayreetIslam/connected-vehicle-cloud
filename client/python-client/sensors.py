a28aec8833aea838293fbca4e88994a51da96574
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2017-03-30 04:38:52 -0400

    Add method to initialize the sensor class

--- client/python-client/sensors.py
+++ client/python-client/sensors.py
@@ -7,7 +7,6 @@ from evdev import InputDevice, categorize, ecodes, list_devices
 from select import select
 import picamera
 from threading import Thread
-from constants import DEVICE_ID
 
 # Logging Settings
 TEMP_H = True
@@ -23,140 +22,127 @@ BASENAME = "Fall"
 WRITE_FREQUENCY = 1
 ENABLE_CAMERA = False
 LOG_AT_START = False
-WebsocketConnection = None
-sense = None
-dev = None
-camera = None
-camera = None
-logging = None
-
-def file_setup(filename):
-    header = []
-    if TEMP_H:
-        header.append("temp_h")
-    if TEMP_P:
-        header.append("temp_p")
-    if HUMIDITY:
-        header.append("humidity")
-    if PRESSURE:
-        header.append("pressure")
-    if ORIENTATION:
-        header.extend(["pitch", "roll", "yaw"])
-    if MAG:
-        header.extend(["mag_x", "mag_y", "mag_z"])
-    if ACCELERATION:
-        header.extend(["accel_x", "accel_y", "accel_z"])
-    if GYRO:
-        header.extend(["gyro_x", "gyro_y", "gyro_z"])
-    header.append("timestamp")
-
-    with open(filename, "w") as f:
-        f.write(",".join(str(value) for value in header) + "\n")
-
-# Function to capture input from the Sense Hat Joystick
-def get_joystick():
-    devices = [InputDevice(fn) for fn in list_devices()]
-    for dev in devices:
-        if dev.name == "Raspberry Pi Sense HAT Joystick":
-            return dev
-
-# Function to collect data from the sense hat and build a string
-def get_sense_data():
-    sense_data = []
-
-    if TEMP_H:
-        sense_data.append(sense.get_temperature_from_humidity())
-
-    if TEMP_P:
-        sense_data.append(sense.get_temperature_from_pressure())
-
-    if HUMIDITY:
-        sense_data.append(sense.get_humidity())
-
-    if PRESSURE:
-        sense_data.append(sense.get_pressure())
-
-    if ORIENTATION:
-        yaw, pitch, roll = sense.get_orientation().values()
-        sense_data.extend([pitch, roll, yaw])
-
-    if MAG:
-        mag_x, mag_y, mag_z = sense.get_compass_raw().values()
-        sense_data.extend([mag_x, mag_y, mag_z])
-
-    if ACCELERATION:
-        x, y, z = sense.get_accelerometer_raw().values()
-        sense_data.extend([x, y, z])
-
-    if GYRO:
-        gyro_x, gyro_y, gyro_z = sense.get_gyroscope_raw().values()
-        sense_data.extend([gyro_x, gyro_y, gyro_z])
-
-    sense_data.append(datetime.now())
-
-    return sense_data
-
-
-def show_state(logging):
-    if logging:
-        sense.show_letter("Y", text_colour=[0, 100, 0])
-    else:
-        sense.show_letter("N", text_colour=[100, 0, 0])
-
-
-def check_input():
-    running = True
-    logging_event = False
-    r, w, x = select([dev.fd], [], [], 0.01)
-    for fd in r:
-        for event in dev.read():
-            if event.type == ecodes.EV_KEY and event.value == 1:
-                logging_event = True
-                if event.code == ecodes.KEY_UP:
-                    if ENABLE_CAMERA and camera.recording:
-                        camera.stop_recording()
-                    running = False
-                if event.code == ecodes.KEY_LEFT:
-                    if ENABLE_CAMERA:
-                        camera.start_recording(
-                            "SenseVid-"+str(datetime.now()) + ".h264"
-                        )
-                        logging_event = False
-
-
-    return logging_event, running
-
-
-def log_data():
-    output_string = ",".join(str(value) for value in sense_data)
-    batch_data.append(output_string)
-
-
-def timed_log():
-    while run:
-        if logging is True:
-            log_data()
-        time.sleep(DELAY)
 
 
 def init(websocket):
-    WebsocketConnection = websocket
-    # Main Program
-    global sense
-    sense = SenseHat()
+    camera = None
+    dev = None
     run = True
-    global logging
     logging = LOG_AT_START
-    show_state(logging)
-    global dev
-    dev = get_joystick()
-    global batch_data
     batch_data = []
 
+    def file_setup(filename):
+        header = []
+        if TEMP_H:
+            header.append("temp_h")
+        if TEMP_P:
+            header.append("temp_p")
+        if HUMIDITY:
+            header.append("humidity")
+        if PRESSURE:
+            header.append("pressure")
+        if ORIENTATION:
+            header.extend(["pitch", "roll", "yaw"])
+        if MAG:
+            header.extend(["mag_x", "mag_y", "mag_z"])
+        if ACCELERATION:
+            header.extend(["accel_x", "accel_y", "accel_z"])
+        if GYRO:
+            header.extend(["gyro_x", "gyro_y", "gyro_z"])
+        header.append("timestamp")
+
+        with open(filename, "w") as f:
+            f.write(",".join(str(value) for value in header) + "\n")
+
+    # Function to capture input from the Sense Hat Joystick
+    def get_joystick():
+        devices = [InputDevice(fn) for fn in list_devices()]
+        for dev in devices:
+            if dev.name == "Raspberry Pi Sense HAT Joystick":
+                return dev
+
+    # Function to collect data from the sense hat and build a string
+    def get_sense_data():
+        sense_data = []
+
+        if TEMP_H:
+            sense_data.append(sense.get_temperature_from_humidity())
+
+        if TEMP_P:
+            sense_data.append(sense.get_temperature_from_pressure())
+
+        if HUMIDITY:
+            sense_data.append(sense.get_humidity())
+
+        if PRESSURE:
+            sense_data.append(sense.get_pressure())
+
+        if ORIENTATION:
+            yaw, pitch, roll = sense.get_orientation().values()
+            sense_data.extend([pitch, roll, yaw])
+
+        if MAG:
+            mag_x, mag_y, mag_z = sense.get_compass_raw().values()
+            sense_data.extend([mag_x, mag_y, mag_z])
+
+        if ACCELERATION:
+            x, y, z = sense.get_accelerometer_raw().values()
+            sense_data.extend([x, y, z])
+
+        if GYRO:
+            gyro_x, gyro_y, gyro_z = sense.get_gyroscope_raw().values()
+            sense_data.extend([gyro_x, gyro_y, gyro_z])
+
+        sense_data.append(datetime.now())
+
+        return sense_data
+
+    def show_state(logging):
+        if logging:
+            sense.show_letter("Y", text_colour=[0, 100, 0])
+        else:
+            sense.show_letter("N", text_colour=[100, 0, 0])
+
+    def check_input():
+        running = True
+        logging_event = False
+        r, w, x = select([dev.fd], [], [], 0.01)
+        for fd in r:
+            for event in dev.read():
+                if event.type == ecodes.EV_KEY and event.value == 1:
+                    logging_event = True
+                    if event.code == ecodes.KEY_UP:
+                        if ENABLE_CAMERA and camera.recording:
+                            camera.stop_recording()
+                        running = False
+                    if event.code == ecodes.KEY_LEFT:
+                        if ENABLE_CAMERA:
+                            camera.start_recording(
+                                "SenseVid-"+str(datetime.now()) + ".h264"
+                            )
+                            logging_event = False
+
+        return logging_event, running
+
+    def log_data():
+        output_string = ",".join(str(value) for value in sense_data)
+        batch_data.append(output_string)
+
+    def timed_log():
+        while run:
+            if logging is True:
+                log_data()
+            time.sleep(DELAY)
+
+    # Main Program
+    sense = SenseHat()
+    show_state(logging)
+    dev = get_joystick()
+
     if BASENAME == "":
-        filename = "SenseLog-"+str(datetime.now()) + ".csv"
+        filename = "SenseLog-"+str(datetime.now())+".csv"
     else:
-        filename = BASENAME+"-"+str(datetime.now()) + ".csv"
+        filename = BASENAME+"-"+str(datetime.now())+".csv"
 
     file_setup(filename)
 
@@ -172,7 +158,6 @@ def init(websocket):
         logging_event, run = check_input()
 
         if logging_event:
-            global logging
             logging = not(logging)
             show_state(logging)
 
@@ -181,24 +166,19 @@ def init(websocket):
 
         if len(batch_data) >= WRITE_FREQUENCY:
             with open(filename, "a") as f:
-                WebsocketConnection.send({
+                websocket.send({
                     "type": "sensor_data",
                     "payload": batch_data,
                     "level": "INFO",
                     "timestamp": time.time() * 1000,
-                    "device_id": DEVICE_ID,
                 })
                 for line in batch_data:
                     f.write(line + "\n")
-                global batch_data
                 batch_data = []
-
 
     with open(filename, "a") as f:
         for line in batch_data:
             f.write(line + "\n")
-            global batch_data
             batch_data = []
-
 
     sense.clear()
