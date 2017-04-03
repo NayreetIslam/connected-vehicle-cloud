761c2d94367a6f4d426638b832863cb73836db0d
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2017-04-02 21:09:37 -0400

    Collect sample data & average it out when reading distance information

--- server/distance_sensor.py
+++ server/distance_sensor.py
@@ -1,50 +1,36 @@
+from hcsr04sensor import sensor
 import time
 import json
 import asyncio
 from threading import Thread
-import RPi.GPIO as GPIO
 from datetime import datetime
-
-GPIO.setmode(GPIO.BCM)
 
 TRIG_PIN = 17
 ECHO_PIN = 27
-DELAY = 0.5
+DELAY = 0.03
 SAFE_DISTANCE = 8
-
-GPIO.setup(TRIG_PIN, GPIO.OUT)
-GPIO.setup(ECHO_PIN, GPIO.IN)
-GPIO.output(TRIG_PIN, GPIO.LOW)
+NUMBER_OF_SAMPLES = 5
 
 
 def start(handleBroadcast, handleStop):
     while True:
-        GPIO.output(TRIG_PIN, GPIO.HIGH)
-        time.sleep(0.00001)
-        GPIO.output(TRIG_PIN, GPIO.LOW)
+        #  Create a distance reading with the hcsr04 sensor module
+        value = sensor.Measurement(TRIG_PIN, ECHO_PIN)
+        raw_measurement = value.raw_distance(sample_size=NUMBER_OF_SAMPLES, sample_wait=DELAY)
 
-        while GPIO.input(ECHO_PIN) == 0:
-            pulse_start = time.time()
-
-        while GPIO.input(ECHO_PIN) == 1:
-            pulse_end = time.time()
-
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
-
-        if distance < SAFE_DISTANCE:
+        # Calculate the distance in centimeters
+        metric_distance = value.distance_metric(raw_measurement)
+        if metric_distance < SAFE_DISTANCE:
             handleStop()
 
         message = {
             "type": "sensor_distance_data",
-            "payload": [distance, str(datetime.now())],
+            "payload": [metric_distance, str(datetime.now())],
             "level": "NOTICE",
             "timestamp": time.time() * 1000,
         }
         print(message)
         handleBroadcast(json.dumps(message))
-        time.sleep(DELAY)
 
 
 def init(handleBroadcast, handleStop):
