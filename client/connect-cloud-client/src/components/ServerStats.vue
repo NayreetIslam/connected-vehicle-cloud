bc2c92930beb21c7c3f72cf55988bebca1ab73c8
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2017-04-02 22:28:07 -0400

    Display distance data

--- client/connect-cloud-client/src/components/ServerStats.vue
+++ client/connect-cloud-client/src/components/ServerStats.vue
@@ -8,7 +8,11 @@
     />
     <LineChartComponent
       :chartData="accelerationData"
-      :options="chartOptions()"
+      :options="chartOptions('accelerationData')"
+    />
+    <LineChartComponent
+      :chartData="distanceData"
+      :options="chartOptions('distanceData')"
     />
   </div>
 </template>
@@ -40,6 +44,21 @@ export default {
         datasets: [
           {
             label: 'Acceleration',
+            data: [],
+            fill: false
+          }
+        ]
+      },
+      distanceData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Distance',
+            data: [],
+            fill: false
+          },
+          {
+            label: 'Safe Distance',
             data: [],
             fill: false
           }
@@ -93,11 +112,53 @@ export default {
         dataset.data.push(parseFloat(accX).toFixed(2))
         dataset.data = dataset.data.slice(-maxPointCoint)
         this.accelerationData = { labels, datasets: [dataset] }
+      } else if (data.data && data.data.type === 'sensor_distance_data') {
+        // process sensor data
+        const { payload } = data.data
+        const safeDistance = 50
+
+        let labels = [...this.distanceData.labels]
+        // set the timestamp, remove the year
+        const maxPointCoint = 30
+        labels.push(payload.slice(-1)[0].replace(/\d{4}-\d{2}-\d{2} /, ''))
+        labels = labels.slice(-maxPointCoint)
+
+        // add data
+        const distance = payload[0]
+        const dataset = {
+          label: 'Distance',
+          data: [...this.distanceData.datasets[0].data],
+          fill: false
+        }
+        const safetyDataset = {
+          label: 'Safe Distance',
+          data: [...this.distanceData.datasets[1].data],
+          fill: false
+        }
+        dataset.data.push(parseFloat(distance).toFixed(2))
+        dataset.data = dataset.data.slice(-maxPointCoint)
+        safetyDataset.data.push(safeDistance)
+        safetyDataset.data = safetyDataset.data.slice(-maxPointCoint)
+        this.distanceData = { labels, datasets: [dataset, safetyDataset] }
       }
     },
 
-    chartOptions () {
-      const dataPoints = this.accelerationData.datasets[0].data.map(v => parseFloat(v))
+    chartOptions (key) {
+      const dataPoints = this[key].datasets[0].data.map(v => parseFloat(v))
+      let min = 0
+      let max = 10
+      let stepSize = 1
+      if (key === 'accelerationData') {
+        min = dataPoints.length ? (Math.min(...dataPoints) - 0.5) : 0
+        max = dataPoints.length ? (Math.max(...dataPoints) + 0.5) : 0
+        stepSize = 0.05
+      } else if (key === 'distanceData') {
+        // Add safe distance point
+        dataPoints.push(50)
+        min = dataPoints.length ? (Math.min(...dataPoints) - 2) : 0
+        max = dataPoints.length ? (Math.max(...dataPoints) + 2) : 0
+        stepSize = 1
+      }
       return {
         animation: false,
         responsive: true,
@@ -105,9 +166,9 @@ export default {
         scales: {
           yAxes: [{
             ticks: {
-              min: dataPoints.length ? (Math.min(...dataPoints) - 0.5) : 0,
-              max: dataPoints.length ? (Math.max(...dataPoints) + 0.5) : 0,
-              stepSize: 0.05
+              min,
+              max,
+              stepSize
             }
           }]
         }
